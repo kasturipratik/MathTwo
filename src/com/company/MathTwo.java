47bebda51d2615c7d5a017ef7bb210611c669474
kasturipratik <pratik.parajuli@yahoo.com>
2018-05-31 15:05:15 -0400

    Add all

---
+++ src/com/company/MathTwo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MathTwo {
+    private static double myothernumber = 1.7938;
+    private static int mynumber = 2;
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("My Number: "+ mynumber);
+        System.out.println("My Other Number: "+ myothernumber);
+
+    }
+}
